ccd4f2f8d0008e8877b5ee235651e267bcbda76f
ofek86 <dimabeofek@gmail.com>
2014-04-10 18:30:38 -0700

    First try!

---
+++ Interpreter/src/backend/RenameMePleaseToWhatYouNeed.java
@@ -0,0 +1,7 @@
+package backend;
+
+/**
+ * Created by Dima on 4/10/2014.
+ */
+public class RenameMePleaseToWhatYouNeed {
+}
---
+++ Interpreter/src/intermediate/DudeForRealRenameMe.java
@@ -0,0 +1,7 @@
+package intermediate;
+
+/**
+ * Created by Dima on 4/10/2014.
+ */
+public class DudeForRealRenameMe {
+}
