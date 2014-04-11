3a80e17e18c3f06b6c3b714bca453137a136f060
ofek86 <dimabeofek@gmail.com>
2014-04-10 18:02:09 -0700

    First try!

---
+++ Interpreter/src/frontend/SchemeInterpreter.java
@@ -0,0 +1,12 @@
+package frontend;
+
+/**
+ * Created by Dima on 4/10/2014.
+ */
+public class SchemeInterpreter
+{
+   public static void main(String[] args)
+   {
+
+   }
+}
---
+++ Interpreter/src/frontend/Token.java
@@ -0,0 +1,35 @@
+package frontend;
+
+/**
+ * Created by Dima on 4/10/2014.
+ */
+public class Token
+{
+    private String identifier;
+    private String attribute;
+
+    public Token()
+    {
+        identifier = "";
+        attribute = "";
+    }
+
+    boolean isValidElem(String Atom) //This function will be part of a class
+    {
+     /*
+    Validates a scanned token by the Scanner class to be a valid Scheme element.
+     */
+        return false;
+    }
+
+    boolean isValidVar(/*Some arguments*/) //This function will be part of a class
+    {
+     /*
+    Validates a scanned token by the Scanner class to be a valid Scheme variable.
+     */
+        return false;
+    }
+
+    //class frontend.Token has a toString method which returns a string ONLY if the token is a valid Scheme token.
+
+}
