1395edfa4a1c7c603899d368b15f52bc15d04bfb
ofek86 <dimabeofek@gmail.com>
2014-04-12 21:23:36 -0700

    Current Work

--- Interpreter/src/frontend/SchemeInterpreter.java
+++ Interpreter/src/frontend/SchemeInterpreter.java
@@ -3,6 +3,23 @@ package frontend;
 /**
  * Created by Dima on 4/10/2014.
  */
+
+/*
+    For Assignment #5, put your parser and scanner classes in the frontend package,
+    and your parse tree and symbol table classes in the intermediate package.
+    The backend package can contain only (for now) your tree printer and a symbol table printer classes.
+    You should read the input (the Scheme source file) from standard input.
+*/
+/**
+ * Created by Dima on 4/10/2014.
+ */
+
+/*
+    identifier (name)
+    number
+    special symbol (+ - * / = etc.)
+    reserved word
+*/
 public class SchemeInterpreter
 {
    public static void main(String[] args)
--- Interpreter/src/intermediate/SymbolTable.java
+++ Interpreter/src/intermediate/SymbolTable.java
@@ -1,7 +1,6 @@
 package intermediate;
 
-/**
- * Created by Dima on 4/10/2014.
- */
-public class SymbolTable {
+public class SymbolTable
+{
+
 }
