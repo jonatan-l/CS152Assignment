ee60f75d2b5bf49eee2844c3bac9ae8218ea374c
ofek86 <dimabeofek@gmail.com>
2014-04-30 20:53:06 -0700

    Working on SchemeFunctionality

---
+++ Interpreter/src/intermediate/SchemeFunctionality.java
@@ -0,0 +1,31 @@
+package intermediate;
+
+public class SchemeFunctionality
+{
+    public SchemeFunctionality(){}
+    public Pair cdr(Pair currentRoot)
+    {
+        if(currentRoot == null)
+        {
+            return null;
+        }
+        if(currentRoot.getCdr().isAtom()) //Checks if an Atom is a Pair(true) pr Atom (false)
+        {
+            return currentRoot.getCdr();
+        }
+        return null;
+    }
+    public Atom car(Pair currentRoot)
+    {
+        if(currentRoot == null)
+        {
+            return null;
+        }
+        if(currentRoot.getCar().isAtom()) //Checks if an Atom is a Token (true) or Pair(false)
+        {
+            return currentRoot.getCar();
+        }
+        return null;
+    }
+
+}
