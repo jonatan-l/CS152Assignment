d0505c81d17e0953749664930a7145f5e06b0d99
ofek86 <dimabeofek@gmail.com>
2014-04-30 18:08:50 -0700

    Working on SymbolTable Stack

--- Interpreter/src/intermediate/SymbolTable.java
+++ Interpreter/src/intermediate/SymbolTable.java
@@ -7,10 +7,16 @@ import java.util.Iterator;
 import java.util.TreeMap;
 
 public class SymbolTable {
-    TreeMap<String, String> map;
+    private TreeMap<String, String> map;
+    private Pair parseTreePair;
 
     public SymbolTable() {
         map = new TreeMap<String, String>();
+        parseTreePair = null;
+    }
+    public void setStackPtr(Pair neededPair) //Simply sets the pointer for every SymbolTable back to the root of a token pair.
+    {
+        parseTreePair = neededPair;
     }
     public void add(String key, String value) {
         map.put(key, value);
@@ -19,7 +25,7 @@ public class SymbolTable {
     public String getValue(String key) {
         return (String) map.get(key);
     }
-
+    public Pair getStackPtr(){ return parseTreePair; }
     public void entries() {
         // TODO Auto-generated method stub
         Collection<?> entrySet = map.entrySet();
--- Interpreter/src/intermediate/SymbolTableStack.java
+++ Interpreter/src/intermediate/SymbolTableStack.java
@@ -5,23 +5,15 @@ import java.util.ArrayList;
 public class SymbolTableStack
 {
    private ArrayList<SymbolTable> symbolStack;
-   private Pair parseTreePair;
+
    public SymbolTableStack()
    {
        symbolStack = new ArrayList<SymbolTable>();
-       parseTreePair = null;
    }
    public void add(SymbolTable newTable)
    {
        symbolStack.add(newTable);
    }
-   public void setStackPtr(Pair neededPair)
-   {
-        /*
-        * Find the current token, then set parseTreePair to it!
-        * */
-   }
-   public Pair getStackPtr(){ return parseTreePair; }
    public SymbolTable getSymbTable(int i)
    {
        return symbolStack.get(i);
