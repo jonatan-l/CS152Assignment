7f53e2879c40ed287485c4ffa9d7deb4d0b3b057
ofek86 <dimabeofek@gmail.com>
2014-05-03 21:48:45 -0700

    Added division and power both integer and floating point.

--- Interpreter/src/backend/Executor.java
+++ Interpreter/src/backend/Executor.java
@@ -152,44 +152,92 @@ public class Executor
 			return Integer.parseInt(((Token)p.getCar()).getValue()) * multiplyIntegerMethod(p.getCdr());
 		}
 	}
-    public float addNumberMethod(Pair p){//use root can add as many as we need to -recursive...base case!!! cdr empty add 0
-
-        if(p == null){
-            return 0;
-        }
-        else if(!p.getCar().isAtom()){
-            return Float.parseFloat(run((Pair)p.getCar()).toString()) + addNumberMethod(p.getCdr());//will run() return int for this?
-        }
-        else
-        {
-            return Float.parseFloat(((Token)p.getCar()).getValue()) + addNumberMethod(p.getCdr());//p.getCar() is atom -integer-
-        }
-    }//is car is a pair - call main run on it
-
-    public float subNumberMethod(Pair p){
-
-        if(p == null){
-            return 0;
-        }
-        else if(!p.getCar().isAtom()){
-            return Float.parseFloat(run((Pair)p.getCar()).toString()) - subNumberMethod(p.getCdr());
-        }
-        else
-        {
-            return Float.parseFloat(((Token)p.getCar()).getValue()) - subNumberMethod(p.getCdr());
-        }
-    }
-
-    public float multiplyNumberMethod(Pair p){
+    public int divideIntegerMethod(Pair p){
         if(p == null){
             return 1;
         }
         else if(!p.getCar().isAtom()){
-            return Float.parseFloat(run((Pair)p.getCar()).toString()) * multiplyNumberMethod(p.getCdr());
+            return Integer.parseInt(run((Pair)p.getCar()).toString()) / multiplyIntegerMethod(p.getCdr());
         }
         else
         {
-            return Float.parseFloat(((Token)p.getCar()).getValue()) * multiplyNumberMethod(p.getCdr());
+            return Integer.parseInt(((Token)p.getCar()).getValue()) / multiplyIntegerMethod(p.getCdr());
+        }
+    }
+    public int powIntegerMethod(Pair p){
+        if(p == null){
+            return 1;
+        }
+        else if(!p.getCar().isAtom()){
+            return (int)Math.pow(Double.parseDouble(run((Pair)p.getCar()).toString()),(double)multiplyIntegerMethod(p.getCdr()));
+        }
+        else
+        {
+            return (int)Math.pow(Double.parseDouble(((Token)p.getCar()).getValue()), (double)multiplyIntegerMethod(p.getCdr()));
+        }
+    }
+    public double addNumberMethod(Pair p){//use root can add as many as we need to -recursive...base case!!! cdr empty add 0
+
+        if(p == null){
+            return 0;
+        }
+        else if(!p.getCar().isAtom()){
+            return Double.parseDouble(run((Pair)p.getCar()).toString()) + addNumberMethod(p.getCdr());//will run() return int for this?
+        }
+        else
+        {
+            return Double.parseDouble(((Token)p.getCar()).getValue()) + addNumberMethod(p.getCdr());//p.getCar() is atom -integer-
+        }
+    }//is car is a pair - call main run on it
+
+    public double subNumberMethod(Pair p){
+
+        if(p == null){
+            return 0;
+        }
+        else if(!p.getCar().isAtom()){
+            return Double.parseDouble(run((Pair)p.getCar()).toString()) - subNumberMethod(p.getCdr());
+        }
+        else
+        {
+            return Double.parseDouble(((Token)p.getCar()).getValue()) - subNumberMethod(p.getCdr());
+        }
+    }
+
+    public double multiplyNumberMethod(Pair p){
+        if(p == null){
+            return 1;
+        }
+        else if(!p.getCar().isAtom()){
+            return Double.parseDouble(run((Pair)p.getCar()).toString()) * multiplyNumberMethod(p.getCdr());
+        }
+        else
+        {
+            return Double.parseDouble(((Token)p.getCar()).getValue()) * multiplyNumberMethod(p.getCdr());
+        }
+    }
+    public double divideNumberMethod(Pair p){
+        if(p == null){
+            return 1;
+        }
+        else if(!p.getCar().isAtom()){
+            return Double.parseDouble(run((Pair)p.getCar()).toString()) / multiplyIntegerMethod(p.getCdr());
+        }
+        else
+        {
+            return Double.parseDouble(((Token)p.getCar()).getValue()) / multiplyIntegerMethod(p.getCdr());
+        }
+    }
+    public double powNumberMethod(Pair p){
+        if(p == null){
+            return 1;
+        }
+        else if(!p.getCar().isAtom()){
+            return Math.pow(Double.parseDouble(run((Pair)p.getCar()).toString()),multiplyIntegerMethod(p.getCdr()));
+        }
+        else
+        {
+            return Math.pow(Double.parseDouble(((Token)p.getCar()).getValue()),multiplyIntegerMethod(p.getCdr()));
         }
     }
 	public boolean andMethod(Pair p){
